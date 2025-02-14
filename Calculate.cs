d0dbfb93eb9d580d3efd64b92fe494fd20182d9a
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-02-14 12:13:28 +0200

    Add files via upload

---
+++ Calculate.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// Класс Calculate
+    /// </summary>
+    class Calculate
+    {
+        /// <summary>
+        /// 1 Вещественное число 
+        /// </summary>
+        private double field_1;
+        /// <summary>
+        /// 2 Вещественное число
+        /// </summary>
+        private double field_2;
+
+        public double Field_1 
+        { 
+            get => field_1;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Error");
+                field_1 = value;
+            }
+        }
+        
+        public double Field_2 { get => field_2; set => field_2 = value; }
+
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        public Calculate() { }
+        /// <summary>
+        /// Конструктор с 1 параметром
+        /// </summary>
+        /// <param name="field_1">Поле_1</param>
+        public Calculate(double field_1)
+        {
+            this.Field_1 = field_1;
+        }
+        /// <summary>
+        /// Конструктор с 2 параметрами 
+        /// </summary>
+        /// <param name="field_1">Поле_1</param>
+        /// <param name="field_2">Поле_2</param>
+        public Calculate(double field_1, double field_2):this(field_1)
+        {
+            this.Field_2 = field_2;
+        }
+        public void Output()
+        {
+            Console.WriteLine($"field_1: {Field_1}\nfield_2: {Field_2}");
+        }
+        public override string ToString()
+        {
+            return $"field_1: {Field_1}\nfield_2: {Field_2}"; 
+        }
+        public double CalculateSquareRoot()
+        {
+            return Math.Sqrt(Field_1 * Field_2);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate calc = new Calculate();
+            calc.Field_1 = 3;
+            calc.Field_2 = 10;
+            calc.CalculateSquareRoot();
+            Calculate calc1 = new Calculate(1);
+            calc1.CalculateSquareRoot();
+            Calculate calc2 = new Calculate(3, 4);
+            calc2.CalculateSquareRoot();
+            Console.WriteLine("Введите первое число:");
+            double asd1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ведите второе число:");
+            double asd2 = double.Parse(Console.ReadLine());
+            Calculate calc3 = new Calculate(asd1, asd2);
+            Console.WriteLine($"{calc3.ToString()}");
+            Console.WriteLine($"{calc3.CalculateSquareRoot()}");
+            Console.Read();
+        }
+    }
+}
